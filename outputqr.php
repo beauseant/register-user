8dc03e331b0b3a4188479d10e5f374581b5f24a4
beauseant <suscripcion@bluethinking.com>
2020-08-03 13:11:00 +0200

    wrong basepath
    Signed-off-by: beauseant <suscripcion@bluethinking.com>

--- outputqr.php
+++ outputqr.php
@@ -7,7 +7,7 @@
   	exit;
   }
 
-	$actual_link =  echo '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
+	$actual_link =  '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
 
 	// $_SERVER['HTTP_HOST']. '/index.php'; 
     
